d53081c0cee9c569e7d35c0afbd0b1ad89bb22db
chasingSublimity <blakemsager@gmail.com>
2016-12-16 11:32:56 -0600

    update

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
 var marvelCharacterEndPoint = 'https://gateway.marvel.com:443/v1/public/characters';
 
 // function to get character data through Marvel API
-function getCharacterInfo(searchTerm) {
+function getCharacterId(searchTerm) {
 	var characterQuery = {
 		name: searchTerm,
 		apikey: 'b5a985cb816977af5a8da412277c108b'
 	};
- 	$.getJSON(marvelCharacterEndPoint, characterQuery, displayResults);
+ 	$.getJSON(marvelCharacterEndPoint, characterQuery, function(object) {
+ 		displayCharacterInfo(object);
+ 		return object.data.results[0].id;
+ 	});
 }
 
-// function to get comic cover image data through Marvel API
-// function getComicBookCovers(searchTerm) {
-// 	var coverQuery = {
-// 		characterID: getCharacterID(searchTerm),
-// 		apikey: 'b5a985cb816977af5a8da412277c108b'
-// 	};
-// 	$.getJSON(marvelEndPoint, coverQuery);
-// }
+// function to use CharacterId to get comic info from marvel API
+function getComicCover(searchTerm) {
+	var comicQuery = {
+		characterId: getCharacterID(searchTerm),
+		format: 'comic',
+		formatType: 'comic',
+		noVariants: true
+	};
+	$.getJSON(marvelCharacterEndPoint, comicQuery, displayComicCovers);
+}
 
-// function to display data
-function displayResults(returnObject) {
-	console.log(returnObject.data);
+// function to display Character data
+function displayCharacterInfo(returnObject) {
+	console.log(returnObject);
 	var apiResults = '';
 	if (returnObject.data.count > 0) {
-		console.log('if statement running');
 		returnObject.data.results.forEach(function(item){
 			apiResults += 
-				'<div class="comic-cover">' +
-					'<img src="' + item.thumbnail.path + '/standard_amazing.' + item.thumbnail.extension + '" class="cover-img">' +
-					'<div class="comic-descrip">' +
+				'<div class="char-info">' +
+					'<img src="' + item.thumbnail.path + '/standard_fantastic.' + item.thumbnail.extension + '" class="char-img">' +
+					'<div class="char-descrip">' +
 						'<h3>' + item.name + '</h3>' +
+						'<p>' + item.description + '</p>' +
 					'</div>' +
 				'</div>'
 			;});
 	} else {
 		apiResults += '<p>No results</p>';
 	}
-	console.log(apiResults);
 	$('.js-search-results').html(apiResults);
 }
+
+// function to display Comic data
 
 // function to listen for submit 
 function watchSubmit() {
@@ -46,7 +52,7 @@ function watchSubmit() {
 		event.preventDefault();
 		var characterName = $(this).find('.js-search-input').val();
 		console.log(characterName);
-		getCharacterInfo(characterName, displayResults);
+		getCharacterId(characterName, displayCharacterInfo);
 		$('.js-search-input').val('');
     $('.js-search-input').focus();
 	});
