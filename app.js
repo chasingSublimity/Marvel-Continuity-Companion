255a12be9f83d7fca45272d8a3433f92ec5d094a
chasingSublimity <blakemsager@gmail.com>
2016-12-18 18:51:43 -0600

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var marvelCharacterEndPoint = 'https://gateway.marvel.com:443/v1/public/characters';
+var marvelCharacterEndPoint = 'http://gateway.marvel.com/v1/public/characters';
 
 // function to get character data through Marvel API
 function getCharacterId(searchTerm) {
@@ -6,29 +6,30 @@ function getCharacterId(searchTerm) {
 		name: searchTerm,
 		apikey: 'b5a985cb816977af5a8da412277c108b'
 	};
- 	$.getJSON(marvelCharacterEndPoint, characterQuery, function(object) {
- 		displayCharacterInfo(object);
- 		return object.data.results[0].id;
- 	});
+ 	var object = $.getJSON(marvelCharacterEndPoint, characterQuery, getComicCover);
 }
 
 // function to use CharacterId to get comic info from marvel API
-function getComicCover(searchTerm) {
+function getComicCover(object) {
+	displayCharacterInfo(object);
+	var id = object.data.results[0].id;
+	var endpoint = 'http://gateway.marvel.com/v1/public/characters/' + id + '/comics';
 	var comicQuery = {
-		characterId: getCharacterID(searchTerm),
+		characterId: id,
 		format: 'comic',
 		formatType: 'comic',
-		noVariants: true
+		noVariants: true,
+		limit: 5,
+		apikey: 'b5a985cb816977af5a8da412277c108b'
 	};
-	$.getJSON(marvelCharacterEndPoint, comicQuery, displayComicCovers);
+	$.getJSON(endpoint, comicQuery, displayComicInfo);
 }
 
 // function to display Character data
 function displayCharacterInfo(returnObject) {
-	console.log(returnObject);
 	var apiResults = '';
 	if (returnObject.data.count > 0) {
-		returnObject.data.results.forEach(function(item){
+		returnObject.data.results.forEach(function(item) {
 			apiResults += 
 				'<div class="char-info">' +
 					'<img src="' + item.thumbnail.path + '/standard_fantastic.' + item.thumbnail.extension + '" class="char-img">' +
@@ -41,18 +42,35 @@ function displayCharacterInfo(returnObject) {
 	} else {
 		apiResults += '<p>No results</p>';
 	}
-	$('.js-search-results').html(apiResults);
+	$('.js-character-container').html(apiResults);
 }
 
 // function to display Comic data
+function displayComicInfo(returnObject) {
+	var apiResults = '';
+	if (returnObject.data.count > 0) {
+		returnObject.data.results.forEach(function(item) {
+			apiResults +=
+				'<div class="comic-info">' +
+					'<img src="' + item.thumbnail.path +'/standard_fantastic.' + item.thumbnail.extension + '" class="comic-img">' +
+					'<div class="comic-descrip">' +
+						'<h3>' + item.title + '</h3>' +
+						'<p>' + item.description + '</p>' +
+					'</div>' +
+				'</div>'
+		;});
+	} else {
+		apiResults += '<p>No results</p>';
+	}
+	$('.js-comic-container').html(apiResults);
+}
 
 // function to listen for submit 
 function watchSubmit() {
 	$('.js-search-form').submit(function (event) {
 		event.preventDefault();
-		var characterName = $(this).find('.js-search-input').val();
-		console.log(characterName);
-		getCharacterId(characterName, displayCharacterInfo);
+		// getCharacterId($(this).find('.js-search-input').val());
+		getCharacterId($(this).find('.js-search-input').val());
 		$('.js-search-input').val('');
     $('.js-search-input').focus();
 	});
