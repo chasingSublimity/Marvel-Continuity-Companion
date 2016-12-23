4e27fdd28aca4c6bc8ed045ab07b06f5d7a1646a
chasingSublimity <blakemsager@gmail.com>
2016-12-23 14:03:41 -0600

    app now only displays 8 cards at a time

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var marvelCharacterEndPoint = 'https://gateway.marvel.com/v1/public/characters';
+var displayComicCardsStartPoint = 0;
 
 // function to get character data through Marvel API
 function getCharacterId(searchTerm) {
@@ -55,18 +56,18 @@ function displayComicCards(returnObject) {
 	var comicCounter = 0;
 	// Checks to see if API returned any results
 	if (returnObject.data.count > 0) {
-		returnObject.data.results.forEach(function(item) {
-		// Url that API returns if no image accompanies the comic object
-		var noImgUrl = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
+		for (i=displayComicCardsStartPoint; i < (displayComicCardsStartPoint + 8); i++) {
+			// Url that API returns if no image accompanies the comic object
+			var noImgUrl = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
 			// Only displays comic with images
-			if (item.thumbnail.path !== noImgUrl) {
+			if (returnObject.data.results[i].thumbnail.path !== noImgUrl) {
 				var htmlFrame = 
 					'<div class="col-3">' +
 						'<div class="cover-container js-cover-container">' +
 							'<div class="comic-info">' +
-								'<img src="' + item.thumbnail.path +'/standard_fantastic.' + item.thumbnail.extension + '" class="comic-img">' +
+								'<a href="' + returnObject.data.results[i].urls[0].url + '">' + '"<img src="' + returnObject.data.results[i].thumbnail.path +'/standard_fantastic.' + returnObject.data.results[i].thumbnail.extension + '" class="comic-img">' + '</a>' +
 								'<div class="comic-descrip">' +
-									'<h3>' + item.title + '</h3>' +
+									'<h3>' + returnObject.data.results[i].title + '</h3>' +
 								'</div>' +
 							'</div>' +
 						'</div>' +
@@ -79,7 +80,7 @@ function displayComicCards(returnObject) {
 				}
 				comicCounter++;
 			}
-		});
+		}
 	} else {
 		apiResults1 += '<p>No Results</p>';
 	}
@@ -88,21 +89,18 @@ function displayComicCards(returnObject) {
 	$('.js-search-results-2').html(apiResults2);
 	// Renders attribution info
 	$('footer').html(returnObject.attributionHTML);
-
 }
+
 // function to listen for submit 
 function watchSubmit() {
 	$('.js-search-form').submit(function (event) {
 		event.preventDefault();
 		$('.app-instructions').hide();
-
 		// Search-result area reset
     $('.js-search-results-1').html('');
 		$('.js-search-results-2').html('');
-
 		// Api calls and rendering
 		getCharacterId($(this).find('.js-search-input').val());
-
 		// Search input reset 
 		$('.js-search-input').val('');
     $('.js-search-input').focus();
